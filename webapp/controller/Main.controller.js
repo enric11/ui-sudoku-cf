23f280bc80908924a448ff5c57b71c4d767e217f
Enric Castella <ecastella@clinic.cat>
2022-05-28 18:45:02 +0000

    change get data method v1

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,32 +10,15 @@ sap.ui.define([
 
         return Controller.extend("ecastella.sudokuui.controller.Main", {
             onInit: function () {
-/*
-            var oModel = this.getOwnerComponent().getModel("sudoku");
-				//oOperation = oModel.bindContext("/catalog/getTodos()");
-                oModel.bindContext("/catalog/getTodos()").execute().then(function () {
-					oModel.refresh();
-					MessageBox.error(this._getText("sourceResetSuccessMessage"));
-				}.bind(this), function (oError) {
-					MessageBox.error(oError.message);
-				}
-			);
-            */
+
             },
             onGetNextAvailableItem: function(oEvent){
                 var that = this;
-                //var oModel = this.getOwnerComponent().getModel("");
                 this.getView().byId("getNextAvailableItem").getObjectBinding().execute().then(function () {
                     // Note: execute does not deliver the results
-                    debugger;
                     var oResults = that.getView().byId("getNextAvailableItem").getObjectBinding().getBoundContext().getObject();
                     
                 });
-
-
-
-                //this.getView().byId("textData").getObjectBinding().execute();
-                debugger;
             }
         });
     });
