dd674d9040e1bd9559d5d32fc47e4a0fad1ab269
Enric Castella <ecastella@clinic.cat>
2022-05-29 13:44:22 +0000

    Add model for sudoku

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
 ],
     /*
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,MessageBox) {
+    function (Controller,MessageBox,JSONModel) {
         "use strict";
 
         return Controller.extend("ecastella.sudokuui.controller.Main", {
@@ -17,6 +18,23 @@ sap.ui.define([
                 this.getView().byId("getNextAvailableItem").getObjectBinding().execute().then(function () {
                     // Note: execute does not deliver the results
                     var oResults = that.getView().byId("getNextAvailableItem").getObjectBinding().getBoundContext().getObject();
+
+                    var sudoku = oResults.value;
+
+                    for (var value in sudoku.board) {
+                        for(var v_height = 0; v_height < sudoku.height; v_height++){  //Altura
+                            for( var v_with = 0; v_with < sudoku.with; v_with++){ // Longitud
+                                console.log(sudoku.board[value][v_with + ( v_height * sudoku.with)] + "|");
+
+                            }
+                            console.log("--------");
+                            // Hi ha un quadrat fet al pasar per aquí
+                        }
+
+                    }
+
+                    var oModel = new JSONModel(sudoku);
+                    that.getView().setModel(oModel,"sudoku");
                     
                 });
             }
